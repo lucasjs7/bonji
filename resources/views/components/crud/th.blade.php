186f0feb0f7866237124d4eddbf478dffa81ec51
Lucas JS <lucas_html@outlook.com>
2022-07-01 15:29:32 -0300

    Foi adicionado componentes para a tabela CRUD

---
+++ resources/views/components/crud/th.blade.php
@@ -0,0 +1,3 @@
+<th {{ $attributes->merge(['class' => 'px-4 pb-2 text-left']) }}>
+    {{ $slot }}
+</th>
---
+++ resources/views/components/crud/thead.blade.php
@@ -0,0 +1,3 @@
+<thead {{ $attributes->merge(['class' => 'border-b border-slate-200']) }}>
+    {{ $slot }}
+</thead>
