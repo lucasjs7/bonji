645af044afda30d9e2da2f5618d38438bfa4aa28
Lucas JS <lucas_html@outlook.com>
2022-07-06 18:07:18 -0300

    Ajuste no componente input:checkbox.

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -5,9 +5,9 @@ $class = '';
 if (in_array($type, ['text', 'number', 'email', 'password', 'tel', 'week', 'url', 'date', 'time', 'search', 'datetime', 'datetime-local', 'month'])) {
     $class = 'w-full h-9 mt-1 px-2 py-1 rounded-[4px] border border-slate-300 font-normal focus:border-blue-600 focus:outline-none focus:ring-0 focus:ring-transparent';
 } elseif ($type == 'checkbox') {
-    $class = '';
+    $class = 'mr-0.5 rounded-[2px] focus:outline-none focus:ring-0 focus:ring-transparent';
 } elseif ($type == 'radio') {
-    $class = '';
+    $class = 'focus:outline-none focus:ring-0 focus:ring-transparent';
 } elseif ($type == 'file') {
     $class = 'w-full mt-1 font-normal focus:border-blue-600 focus:outline-none focus:ring-0 focus:ring-transparent';
 }
